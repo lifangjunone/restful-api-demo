575591c06e5e9a1783ea972b4f2529312e9435d6
lifangjun <lifj@ebondhm.com>
2022-10-18 17:42:53 +0800

    feature: registry grpc server

--- apps/service_registry/svc_registry.go
+++ apps/service_registry/svc_registry.go
@@ -1,6 +1,7 @@
 package service_registry
 
 import (
+	_ "github.com/lifangjunone/restful-api-demo/apps/book/impl"
 	_ "github.com/lifangjunone/restful-api-demo/apps/host/http"
 	_ "github.com/lifangjunone/restful-api-demo/apps/host/impl"
 )
